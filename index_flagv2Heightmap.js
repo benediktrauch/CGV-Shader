4f3911f7b835519d9097df76597d132413d5edcf
Benedikt Rauch <benediktrauch@users.noreply.github.com>
2017-07-15 19:32:34 +0200

    version 2 w/ height map

--- index_flagv2Heightmap.js
+++ index_flagv2Heightmap.js
@@ -32,10 +32,16 @@ function startGame(slider) {
         var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 3, Math.PI / 4 , 20, BABYLON.Vector3.Zero(), scene);
 
         camera.attachControl(canvas);
+
+        // Light
+        var spot = new BABYLON.PointLight("spot", new BABYLON.Vector3(0, 30, 10), scene);
+        spot.diffuse = new BABYLON.Color3(1000, 1000, 1000);
+        spot.specular = new BABYLON.Color3(10, 10, 10);
  
         // Creating flag
-        var flag = BABYLON.Mesh.CreateGround("Flag", 10, 10, 32, scene, true);
- 
+        //var flag = BABYLON.Mesh.CreateGround("Flag", 10, 10, 32, scene, true);
+        var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "worldHeightMap.jpg", 200, 200, 250, 0, 10, scene, false);
+
         var flagMaterial = new BABYLON.ShaderMaterial("flag", scene, {
             vertexElement: "vertexShaderCode",
             fragmentElement: "fragmentShaderCode"
@@ -46,14 +52,27 @@ function startGame(slider) {
         });
         flagMaterial.setTexture("textureSampler1", new BABYLON.Texture("earth.jpg", scene));
         flagMaterial.setTexture("textureSampler2", new BABYLON.Texture("moon.jpg", scene));
-        flagMaterial.setTexture("textureSampler3", new BABYLON.Texture("HRW.jpg", scene));
+        flagMaterial.backFaceCulling = true;
 
 		var time = 1.0;
 
         flagMaterial.setFloat("time", 0.0);
         flagMaterial.setFloat("textureValue", 0.0);
 
-        flag.material = flagMaterial;
+        ground.material = flagMaterial;
+
+        //Sphere to see the light's position
+        var sun = BABYLON.Mesh.CreateSphere("sun", 10, 4, scene);
+        sun.material = new BABYLON.StandardMaterial("sun", scene);
+        sun.material.emissiveColor = new BABYLON.Color3(10, 10, 0);
+
+        //Sun animation
+        scene.registerBeforeRender(function () {
+            sun.position = spot.position;
+            spot.position.x -= 0.5;
+            if (spot.position.x < -90)
+                spot.position.x = 100;
+        });
  
         engine.runRenderLoop(function () {
             var slierValue = getSlider();
